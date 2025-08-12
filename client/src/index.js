07caf78639e29157e12f8b1b2e9b5bb9b23c7243
Kiruthick-02 <kiruthickrn@gmail.com>
2025-08-12 11:03:56 +0530

    env updated

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 
 // Import the CSS for the image cropping component
-import 'react-image-crop/dist/ReactCrop.css';
+//import 'react-image-crop/dist/ReactCrop.css';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
