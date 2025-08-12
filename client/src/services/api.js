5924ccefc161a25bf93be6bb971fc58cd6686c89
Kiruthick-02 <kiruthickrn@gmail.com>
2025-08-12 11:21:45 +0530

    .env  is updated

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-
-
 const api = axios.create({
-  baseURL: 'https://splitit-smart-bill-splitter.onrender.com/', // Replace with your Render URL
+  baseURL: 'http://localhost:5001/api', // Your backend API URL
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
+
 
 export default api;
