f1e9abb2a764f2d9ae258f0e6a18ee0cf9736b92
Kiruthick-02 <kiruthickrn@gmail.com>
2025-08-12 11:13:15 +0530

    Update API baseURL to deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:5001/api', // Your backend API URL
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+
 
 const api = axios.create({
   baseURL: 'https://splitit-smart-bill-splitter.onrender.com/', // Replace with your Render URL
